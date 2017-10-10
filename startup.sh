db0e2358a28baf4fb099593116e56138e39f849b
Jan Blasko <honza@lucien144.net>
2017-10-10 11:07:30 +1100

    Do not reboot unattended updates 💡
    However, keep that settings in conf for further change.
    
    Signed-off-by: Jan Blasko <honza@lucien144.net>

--- startup.sh
+++ startup.sh
@@ -37,7 +37,7 @@ apt-get -y install unattended-upgrades python-software-properties software-prope
 apt-get -y install mc htop
 
 # Install security updates automatically
-echo -e "APT::Periodic::Update-Package-Lists \"1\";\nAPT::Periodic::Unattended-Upgrade \"1\";\n" > /etc/apt/apt.conf.d/20auto-upgrades
+echo -e "APT::Periodic::Update-Package-Lists \"1\";\nAPT::Periodic::Unattended-Upgrade \"1\";\nUnattended-Upgrade::Automatic-Reboot \"false\";\n" > /etc/apt/apt.conf.d/20auto-upgrades
 /etc/init.d/unattended-upgrades restart
 
 # Change the timezone
